9a21cd7fa2d5b757721685a97634712031c505b9
ichigatsu <firstmoon1986@gmail.com>
2025-05-19 21:24:29 +0900

    一巻の半分くらいまで追加

---
+++ app.py
@@ -0,0 +1,25 @@
+import folium
+import json
+import streamlit as st
+from streamlit_folium import st_folium
+
+map = folium.Map(location=(35.78723722411855, 116.09248508465616), zoom_start=6, min_zoom=4, control_scale=True)
+
+with open("prefecture.json", mode="r", encoding="utf-8") as f:
+    prefectures = json.loads(f.read())
+
+for prefecture in prefectures:
+    # lat = prefecture['lat']
+    # lon = prefecture['lon']
+    folium.Marker(
+    location=[prefecture['lat'], prefecture['lon']],
+    tooltip=prefecture['name_Song'],
+    popup=folium.Popup(
+        prefecture['description'],
+        max_width=300, offset=(0, -20),
+    ),
+    # icon=folium.Icon(color="red"),
+).add_to(map)
+
+st.subheader('北方水滸伝　地図')
+st_data = st_folium(map, width=900, height=700)
